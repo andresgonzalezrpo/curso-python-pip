a3608af1efc61255f483d8ef07dc569a9d787a1f
Andrés González <afgonzalezr9003@gmail.com>
2024-07-03 14:59:13 -0500

    World population with pandas

--- app/main.py
+++ app/main.py
@@ -10,8 +10,10 @@ user_options = ('population_by_country', 'population_by_continent' ,'word_popula
 
 def run(option): 
 
-  data = read_csv.read_csv('data.csv')
+  
+  
   if option == 'population_by_country':
+    data = read_csv.read_csv('data.csv')  
     country = input('Type country => ')    
     result = utils.population_by_country(data, country)
     if len(result) > 0:
@@ -20,9 +22,8 @@ def run(option):
       charts.generate_bar_chart(country,labels,values)
     else:
       print('Country not found') 
-      
   elif option == 'population_by_continent':
-    df = pd.read_csv('data.csv')    
+    df = pd.read_csv('data.csv')
     continent = input('Ingrese el continente: ')
     df = df[df['Continent'] == continent]
     countrys = df['Country/Territory'].values
@@ -31,9 +32,10 @@ def run(option):
     charts.generate_pie_chart(countrys,percentages,continent)
 
   elif option == 'word_population':
-    world_population_percent = list(map(lambda item: item['World Population Percentage'], data))
-    countrys = list(map(lambda item: item['Country/Territory'], data))
-    charts.generate_pie_chart(countrys,world_population_percent)
+    df = pd.read_csv('data.csv')
+    countrys = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values    
+    charts.generate_pie_chart(countrys,percentages)
   
   """
   data = read_csv.read_csv('data.csv')
